6104ca38ab7591ef9465a2506db7f62d88e6f744
alessandro-guttadauro <alegutta23@gmail.com>
2026-05-05 09:47:02 +0200

    Completamento main e scoring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
+import pygame
 from generator import generate_trial
 rng = random.Random(42)
 trial_di_test = generate_trial(rng)
 print(trial_di_test)
+pygame.init()
--- scoring.py
+++ scoring.py
@@ -1,2 +1,5 @@
 def apply_answer(score:int, is_correct: bool) -> int:
-    if is_correct : 
+    if is_correct:
+        return score +10
+    else :
+        return score -5  
