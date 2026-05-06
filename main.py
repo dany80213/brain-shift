90a3c1098d17bee0bfa708330c982e482ca4ca39
MattePavons <paopavons@gmail.com>
2026-05-06 08:56:25 +0200

    Aggiornamento codice Brain-Shift

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ rng = random.Random(42)
 trial_di_test = generate_trial(rng)
 print(trial_di_test)
 pygame.init()
+WIDTH, HEIGHT = 800,600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+runnig = True 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+pygame.quit()
